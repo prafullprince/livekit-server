1cd56419f92c8ed5f9eb99aa691accef5780b1f7
Prafull Prince <prafullprince12@gmail.com>
2025-06-18 18:24:08 +0530

    build

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -13,6 +22,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const http_1 = __importDefault(require("http"));
 const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const socket_io_1 = require("socket.io");
 // routes
 const auth_routes_1 = __importDefault(require("./routes/auth.routes"));
 const category_routes_1 = __importDefault(require("./routes/category.routes"));
@@ -23,153 +33,102 @@ const payment_routes_1 = __importDefault(require("./routes/payment.routes"));
 // dbConnect
 const mongodb_1 = __importDefault(require("./config/mongodb"));
 const cloudinary_1 = require("./config/cloudinary");
-const ws_1 = require("ws");
+const redis_1 = __importDefault(require("./config/redis"));
 const order_controllers_1 = require("./controllers/order.controllers");
 dotenv_1.default.config(); // Load environment variables
 // Create Express server
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
-const wss = new ws_1.WebSocketServer({ server });
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
 // memory for socket
 exports.chatRoom = new Map();
 exports.userMap = new Map();
 exports.senderSocket = null;
 exports.receiverSocket = null;
 // allowed origins
-const allowedOrigins = ['https://www.rentabuddy.in/', 'https://rent-a-buddy-client.vercel.app/'];
-// wesocket logic
-wss.on("connection", (socket) => {
-    console.log("connected");
-    socket.on("message", (data) => {
-        console.log("data:::", data);
-        // parsedData
-        if (!data) {
+const allowedOrigins = [
+    "https://www.rentabuddy.in/",
+    "https://rent-a-buddy-client.vercel.app/",
+];
+// socket logic
+io.on("connection", (socket) => {
+    // Register user
+    socket.on("register", (_a) => __awaiter(void 0, [_a], void 0, function* ({ userId }) {
+        // Store userId -> socketId
+        yield redis_1.default.set(`user:${userId}`, socket.id);
+        // Store socketId -> userId
+        yield redis_1.default.set(`socket:${socket.id}`, userId);
+        // send toast of online
+        io.to(socket.id).emit("online");
+    }));
+    // registerUserInChat
+    socket.on("registerUserInChat", (_a) => __awaiter(void 0, [_a], void 0, function* ({ chatId, userId }) {
+        console.log("registerUserInChat");
+        // store chatId -> userId and socket
+        yield redis_1.default.hset(`chat:${chatId}`, userId, socket.id);
+        // store socketId -> userId
+        yield redis_1.default.hset(`sockets:${socket.id}`, "userId", userId);
+        // track all chat this socket joined
+        yield redis_1.default.sadd(`sockets:${socket.id}:chats`, chatId);
+        // send toast of online
+        io.to(socket.id).emit("Connected");
+    }));
+    // openChat
+    socket.on("openChat", (_a) => __awaiter(void 0, [_a], void 0, function* ({ chatId, userId }) {
+        console.log("openChat");
+        if (!chatId || !userId)
             return;
-        }
-        const parsedData = JSON.parse(data.toString());
-        console.log("parsedData", parsedData);
-        // register user
-        if (parsedData.type === "register") {
-            (0, order_controllers_1.registerUserInChatRoom)(parsedData, socket);
-        }
-        // openChat
-        if (parsedData.type === "openChat") {
-            console.log("openChat");
-            exports.userMap === null || exports.userMap === void 0 ? void 0 : exports.userMap.set(parsedData.payload.userId, parsedData.payload.chatId);
-        }
-        // closeChat
-        if (parsedData.type === "closeChat") {
-            console.log("closeChat");
-            exports.userMap === null || exports.userMap === void 0 ? void 0 : exports.userMap.delete(parsedData.payload.userId);
-        }
-        // ping
-        if (parsedData.type === "ping") {
-            socket.send(JSON.stringify({ type: "pong" }));
+        // mark this user is online in this chat
+        yield redis_1.default.set(`activeChat:${userId}`, chatId);
+    }));
+    // closeChat
+    socket.on("closeChat", (_a) => __awaiter(void 0, [_a], void 0, function* ({ chatId, userId }) {
+        console.log("closeChat");
+        if (!chatId || !userId)
             return;
-        }
-        // sendMessage
-        if (parsedData.type === "sendMessage") {
-            (0, order_controllers_1.sendMessage)(parsedData);
-        }
-        // requestOrder
-        if (parsedData.type === "requestOrder") {
-            console.log("requestOrder");
-            (0, order_controllers_1.requestOrder)(parsedData, socket);
-        }
-        // acceptOrder
-        if (parsedData.type === "acceptOrder") {
-            console.log("acceptOrder");
-            (0, order_controllers_1.acceptOrder)(parsedData, socket);
-        }
-        // reloadChat
-        if (parsedData.type === "reloadChatPage") {
-            console.log("reloadChatPagesd,f sdm fdms fmndsfnm");
-            (0, order_controllers_1.reloadChatPage)(parsedData, socket);
-        }
-        // markAsRead
-        if (parsedData.type === "markAsRead") {
-            console.log("markAsRead");
-            (0, order_controllers_1.markAsRead)(parsedData, socket);
-        }
-        // no of unseenMessages
-        if (parsedData.type === "unseenMessages") {
-            console.log("unseenMessages");
-            (0, order_controllers_1.unseenMessages)(parsedData, socket);
-        }
-        // unseenMessages of particular chatId
-        if (parsedData.type === "unseenMessageOfParticularChatIdOfUser") {
-            (0, order_controllers_1.unseenMessageOfParticularChatIdOfUser)(parsedData, socket);
-        }
-        // fetchAllChat
-        if (parsedData.type === "fetchAllChat") {
-            console.log("fetchAllChat");
-            (0, order_controllers_1.fetchUserChats)(parsedData, socket);
-        }
-        // createOffer
-        else if (parsedData.type === "createOffer") {
-            console.log("createOffer");
-            const { chatId, userId, offer } = parsedData.payload;
-            // get participants
-            const participants = exports.chatRoom.get(chatId);
-            if (!participants)
-                return;
-            // get receiver socket
-            const receiverSocket = participants.get(userId);
-            // if receiverSocket is not available return
-            if (!receiverSocket)
-                return;
-            // send offer to receiver
-            receiverSocket.send(JSON.stringify({ type: "createOffer", payload: offer }));
-        }
-        // createAnswer
-        else if (parsedData.type === "createAnswer") {
-            console.log("createAnswer");
-            const { chatId, userId, sdp } = parsedData.payload;
-            // get participants
-            const participants = exports.chatRoom.get(chatId);
-            if (!participants)
-                return;
-            // get sender socket
-            const senderSocket = participants.get(userId);
-            // if senderSocket is not available return
-            if (!senderSocket)
-                return;
-            // send offer to sender
-            senderSocket.send(JSON.stringify({ type: "createAnswer", payload: sdp }));
-        }
-        // add-ice-candidate
-        else if (parsedData.type === "add-ice-candidate") {
-            console.log("add-ice-candidate");
-            const { chatId, userId, candidate } = parsedData.payload;
-            // check socket of which one sender or receiver
-            // get participants
-            const participants = exports.chatRoom.get(chatId);
-            if (!participants)
-                return;
-            // get receiver socket
-            const receiverSocket = participants.get(userId);
-            // if receiverSocket is not available return
-            if (!receiverSocket)
-                return;
-            // add-ice-candidate over receiver
-            receiverSocket.send(JSON.stringify({ type: "add-ice-candidate", payload: candidate }));
-        }
-        else if (parsedData.type === "endCall") {
-            console.log("endCall");
-            const { chatId, userId } = parsedData.payload;
-            // get participants
-            const participants = exports.chatRoom.get(chatId);
-            if (!participants)
-                return;
-            // get sender socket
-            const receiverSocket = participants.get(userId);
-            // if senderSocket is not available return
-            if (!receiverSocket)
-                return;
-            // endCall over sender
-            receiverSocket.send(JSON.stringify({ type: "endCall" }));
-        }
+        yield redis_1.default.del(`activeChat:${userId}`);
+    }));
+    // requestOrder
+    socket.on("requestOrder", (formData) => {
+        console.log("requestOrder");
+        (0, order_controllers_1.requestOrder)(formData, socket, io);
     });
+    // fetchAllChat
+    socket.on("fetchAllChat", ({ userId }) => {
+        console.log("fetchAllChat");
+        (0, order_controllers_1.fetchUserChats)(userId, socket, io);
+    });
+    // sendMessage
+    socket.on("sendMessage", (messagePayload) => {
+        console.log("sendMessage");
+        (0, order_controllers_1.sendMessage)(messagePayload, io);
+    });
+    // On disconnect -> remove user from room
+    socket.on("disconnect", () => __awaiter(void 0, void 0, void 0, function* () {
+        console.log("disconnect");
+        const userId = yield redis_1.default.get(`socket:${socket.id}`);
+        const chatUserId = yield redis_1.default.hget(`sockets:${socket.id}`, "userId");
+        const chatIds = yield redis_1.default.smembers(`sockets:${socket.id}:chats`);
+        // remove registr user
+        if (userId) {
+            yield redis_1.default.del(`user:${userId}`);
+            yield redis_1.default.del(`socket:${socket.id}`);
+        }
+        // remove register chatRoom
+        // -> remove user from all chat
+        if (chatUserId && chatIds) {
+            chatIds.forEach((chatId) => __awaiter(void 0, void 0, void 0, function* () {
+                yield redis_1.default.hdel(`chat:${chatId}`, chatUserId);
+            }));
+        }
+        // -> remove socket from all chat
+        yield redis_1.default.del(`sockets:${socket.id}:chats`);
+        yield redis_1.default.hdel(`sockets:${socket.id}`, "userId");
+    }));
 });
 // rate limit
 const globalLimiter = (0, express_rate_limit_1.default)({
@@ -196,17 +155,18 @@ app.use((0, express_fileupload_1.default)({
     preserveExtension: true,
 }));
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true
-}));
+app.use((0, cors_1.default)()
+//   {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true
+// }
+);
 app.use((0, helmet_1.default)());
 app.use((0, compression_1.default)());
 // app.use(morgan("combined")); // Logs requests
